87e4a3e8a4de15673c278a190786b77c52a3c615
Jeong <JeongYK92@gmail.com>
2020-06-17 23:52:54 +0900

    #2.1 Recsable Components with JSX + Props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import Potato from './Potato';
+
+// jsx + props
+
+// function Food(props) {
+  // return <h1>I like {props.fav}.</h1>;
+function Food({ fav }) {
+  // console.log(props);
+  return <h1>I like {fav}.</h1>;
+}
 
 function App() {
   return (
-    <div class="App">
+    <div>
       <h1>Hello?</h1>
-      <Potato />
+      {/* Food component에 fav라는 이름의 property(prop)를 kimchi라는 value로 할당 */}
+      {/* component는 대문자로 시작, props는 첫 번째 args로 전달 */}
+      <Food fav="kimchi" something={true} />
+      <Food fav="ramen" something={true} />
     </div>
   );
 }
