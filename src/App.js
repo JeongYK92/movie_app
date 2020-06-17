787adca942417c8cf614e2fe3e0c9be8fd485e6f
Jeong <JeongYK92@gmail.com>
2020-06-18 00:48:33 +0900

    #3.0 Class Components and State

--- src/App.js
+++ src/App.js
@@ -1,51 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-// Protection with PropTypes
-// search : prop types react
+// Class Components and State
+
 /*
-npm install prop-types --save
+react는 자동으로 class component의 render method를 실행
+state는 obj.
 */
 
-function Food({ name, picture, rating }) {
-  return (<div>
-    <h2>I like {name}</h2>
-    <h4>Rating : {rating} / 5.0</h4>
-    <img src={picture} alt={name} />
-  </div>);
-}
+class App extends React.Component {
+  state = {
+    count: 0
+  };
 
-// isRequired = 해당 prop은 반드시 있어야 한다.(필수)
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  // rating: PropTypes.string.isRequired // Failed prop type: Invalid prop `rating` of type `number` supplied to `Food`, expected `string`
-  rating: PropTypes.number
-};
+  add = () => {
+    console.log("add");
+  };
+  
+  minus = () => {
+    console.log("minus");
+  };
 
-const foodILike = [
-  {
-    id: 1,
-    name: "Kimchi",
-    image:
-      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fqph.fs.quoracdn.net%2Fmain-qimg-44ac2cd7c76daaa5ffaffe1997f69584&f=1&nofb=1",
-    rating: 4.9
-  },
-  {
-    id: 2,
-    name: "Ramen",
-    image:
-      "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fcfile24.uf.tistory.com%2Fimage%2F1126D0405101E9FE038AB4&f=1&nofb=1",
-    rating: 4.5
+  render() {
+    return (
+      <div>
+        <h1>The number is {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
   }
-]
-
-function App() {
-  return (
-    <div>
-      {foodILike.map(dish => <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />)}
-    </div>
-  );
 }
 
 export default App;
