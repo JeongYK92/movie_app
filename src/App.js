531cd5d882f2244664e90137c2ecad1947c3e416
Jeong <JeongYK92@gmail.com>
2020-06-18 16:55:39 +0900

    #6.1 Building the Router: HashRouter, Route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
+import { HashRouter, Route } from 'react-router-dom';
+import Home from "./routes/Home";
+import About from "./routes/About";
 
-// Routing!
-// react-router-dom
+// Building the Router
 /*
-  npm install react-router-dom --save
-  react에서 navigation bar를 만들어줌
+  Route component에서 중요한 props는 path와 exact, component.
+    - path : 라우팅 경로
+    - exact : 경로가 정확해야만(True) render
+    - component : render page
 */
 
 function App() {
-  return <span>something</span>;
+  return <HashRouter>
+    {/* 이 경우 /about이라 하면 /도 render, /about도 render */}
+    {/* <Route path="/" component={Home} />
+    <Route path="/about" component={About} /> */}
+    {/* 이 경우 /about이면 /about만! */}
+    <Route path="/" exact={true} component={Home} />
+    <Route path="/about" exact={true} component={About} />
+    
+  </HashRouter>;
 }
 
 export default App;
