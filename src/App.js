b8c848f099c0554c53b127fb6bb89f190233610b
Jeong <JeongYK92@gmail.com>
2020-06-18 15:36:27 +0900

    #4.3 Adding Genres: map, key

--- src/App.js
+++ src/App.js
@@ -30,11 +30,11 @@ class App extends React.Component {
     return(
       <section className="container">
         {isLoading ? (
-          <div class="loader">
-            <span class="loader__text">"Loading..."</span>
+          <div className="loader">
+            <span className="loader__text">"Loading..."</span>
           </div>
         ) : (
-          <div class="movies">
+          <div className="movies">
             {movies.map(movie => (
                 <Movie
                   key={movie.id}
@@ -43,6 +43,7 @@ class App extends React.Component {
                   title={movie.title}
                   summary={movie.summary}
                   poster={movie.medium_cover_image}
+                  genres={movie.genres}
                 />
             ))}
           </div>
--- src/Movie.js
+++ src/Movie.js
@@ -2,16 +2,21 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./Movie.css";
 
-// 꼭 class component가 될 필요는 없다.
+// label tag 속성에는 for가 있다. 이는 js와 혼동되기 때문에 htmlFor로 수정해야한다.
 
-function Movie({ year, title, summary, poster }) {
-  return <div class="movie">
+function Movie({ year, title, summary, poster, genres }) {
+  return <div className="movie">
     <img src={poster} alt={title} title={title} />
-    <div class="movie__data">
+    <div className="movie__data">
       {/* <h3 class="movie__title" style={{backgroundColor: "red"}}>{title}</h3> */}
-      <h3 class="movie__title">{title}</h3>
-      <h5 class="movie__year">{year}</h5>
-      <p class="movie__summary">{summary}</p>
+      <h3 className="movie__title">{title}</h3>
+      <h5 className="movie__year">{year}</h5>
+      <ul className="genres">
+        {genres.map((genre, index) => (
+          <li key={index} className="genres__genre">{genre}</li>
+        ))}
+      </ul>
+      <p className="movie__summary">{summary}</p>
     </div>
   </div>;
 }
@@ -21,7 +26,8 @@ Movie.propTypes = {
   year: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
-  poster: PropTypes.string.isRequired
+  poster: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired
 }
 
 export default Movie;
