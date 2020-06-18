184031901e7fbf7638e0bf5c674c869485781de6
Jeong <JeongYK92@gmail.com>
2020-06-18 15:55:44 +0900

    Updates

---
+++ precache-manifest.d56fcacf0c97d7f046ca495dcc2f3c14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "970afda70ba8ed890797067568cf5dbb",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "1c985258239521ce80f0",
+    "url": "/movie_app/static/css/main.ba91ce6e.chunk.css"
+  },
+  {
+    "revision": "78a8c6d4641937053d35",
+    "url": "/movie_app/static/js/2.19735fbb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.19735fbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c985258239521ce80f0",
+    "url": "/movie_app/static/js/main.eeabf361.chunk.js"
+  },
+  {
+    "revision": "793037b19926e1970ea2",
+    "url": "/movie_app/static/js/runtime-main.8469f341.js"
+  }
+]);
