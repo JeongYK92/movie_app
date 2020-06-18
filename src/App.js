be6f3091432c0deab16534dd6c11347b22819efe
Jeong <JeongYK92@gmail.com>
2020-06-18 15:58:37 +0900

    #5.0 Deploying to Github Pages

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import axios from 'axios';
 import Movie from './Movie';
 import "./App.css";
 
-// Cutting the summary
-
 class App extends React.Component {
   state = {
     isLoading: true,
--- src/Movie.js
+++ src/Movie.js
@@ -2,13 +2,10 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./Movie.css";
 
-// label tag 속성에는 for가 있다. 이는 js와 혼동되기 때문에 htmlFor로 수정해야한다.
-
 function Movie({ year, title, summary, poster, genres }) {
   return <div className="movie">
     <img src={poster} alt={title} title={title} />
     <div className="movie__data">
-      {/* <h3 class="movie__title" style={{backgroundColor: "red"}}>{title}</h3> */}
       <h3 className="movie__title">{title}</h3>
       <h5 className="movie__year">{year}</h5>
       <ul className="movie__genres">
